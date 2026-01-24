87e910e27a5fcea746e5dad5bf3d59ae0b1e1564
Esteban Lopez <estebanlopez@Estebans-MacBook-Air.local>
2026-01-23 21:34:23 -0500

    chore: corrects faqs copy.

--- src/routes/corporativeContent/models/faqs.ts
+++ src/routes/corporativeContent/models/faqs.ts
@@ -2,10 +2,10 @@ const faqs = [
   { title: '¿Cómo funciona?', content: 'Haz click en el botón "¡Empieza aquí!" para visitar nuestro WhatsApp. cuéntanos si alguien te refirió y qué necesidad de liquidéz tienes. Envíanos un par de documentos y a vuelta de correo recibirás un pagaré que puedes aceptar o declinar. Al poco tiempo tendrás el dinero en tu cuenta.'},
   { title: '¿Cuáles son los plazos?', content: 'Los plazos pueden ser de hasta 12 meses, con posibilidad de prórroga.'},
   { title: '¿Cuánto tiempo tarda el proceso?', content: 'El proceso puede tomar de 1 a 5 días hábiles.'},
-  { title: '¿Quién puede acceder?', content: 'Ciudadanos colombianos mayores de edad y con contrato laboral vigente. Nuestro modelo privilegia contratistas que necesitan liquidez.'},
+  { title: '¿Quién puede acceder?', content: 'Ciudadanos colombianos mayores de edad y con contrato laboral o de prestación de servicios. Nuestro modelo privilegia contratistas que necesitan liquidez.'},
   { title: '¿Qué tasas manejan?', content: 'Las tasas varían según el resultado de nuestra evaluación. Buscamos ofrecerte tasas competitivas y, sobre todo, legales. Queremos que crezcas, no que te ahogues en deudas interminables.'},
   { title: '¿Cuánto me prestan?', content: 'Para usuarios nuevos y que no vienen referidos por un usuario antiguo, prestamos hasta $1,000,000. Para usuarios referidos por otros usuarios, el monto puede ser de hasta $3,000,000. Para usuarios recurrentes, el monto puede ser de hasta $5,000,000. Montos mayores mediante solicitud especial.'},
-  { title: '¿Qué documentos piden?', content: 'Tu documento de identidad, contrato laboral vigente, prueba de dirección de domicilio y certificación bancaria. Además, algunos datos personales como correo electrónico y teléfono de contacto.'},
+  { title: '¿Qué documentos piden?', content: 'Tu documento de identidad, contrato laboral o de prestación de servicios, prueba de dirección de domicilio y certificación bancaria. Además, algunos datos personales como correo electrónico y teléfono de contacto.'},
   { title: '¿Solicitan prenda?', content: 'Para algunos préstamos sí, dependiendo del monto solicitado y la historia del cliente con nosotros. Para todos los casos, solicitamos un pagaré firmado.'},
   { title: '¿En cuánto tiempo recibo el dinero?', content: 'Depende de la rapidez para diligenciar los documentos. Usualmente tardamos entre 1 y 5 días después de firmado el contrato.'},
 ]
